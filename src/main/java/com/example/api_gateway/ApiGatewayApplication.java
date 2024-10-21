64e08df20c2e3a4161d92d829fc92e063c085d47
fatmaamr22 <fatmaamro34@gmail.com>
2024-10-21 13:07:25 +0300

    Edit Properties file

--- src/main/java/com/example/api_gateway/ApiGatewayApplication.java
+++ src/main/java/com/example/api_gateway/ApiGatewayApplication.java
@@ -1,5 +1,6 @@
 package com.example.api_gateway;
 
+import com.netflix.discovery.EurekaNamespace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
